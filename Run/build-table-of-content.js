c2104c58bde925f6f850a13898f07019b357fa9a
Darren Looby <darren.looby@googlemail.com>
2019-02-13 13:13:40 +0000

    Next and previous links

--- Run/build-table-of-content.js
+++ Run/build-table-of-content.js
@@ -10,12 +10,11 @@ const chapters = fs.readdirSync(chaptersPath)
 const misc = fs.readdirSync(miscPath)
 const tocFile = fs.existsSync(tocPath) ? fs.readFileSync(tocPath, 'utf8') : `# ${tocTitle} \n\n`
 
-console.log('tocFile', tocFile);
-console.log('chapters', chapters);
-console.log('misc', misc);
+const tocRegex = /^(# Table Of Content)$([\w\W]*)/gm;
+const nextorprevRegex = /^(Next|Previous) \[.*\]\(.*\)$/gm;
+const nextRegex = /^Next \[.*\]\(.*\)$/gm;
+const prevRegex = /^Previous \[.*\]\(.*\)$/gm;
 
-const regex = /^(# Table Of Content)$([\w\W]*)/gm;
-console.log('regex', tocFile.match(regex));
 const toc = []
 
 const makeTitle = ({n, t, p}) => {
@@ -24,22 +23,76 @@ const makeTitle = ({n, t, p}) => {
   return `[${n}. ${title}](${rel})`
 }
 
+const makeItem = ({n, t, p}) => {
+  const title = makeTitle({n, t, p})
+  const path = p
+  return {
+    title,
+    path
+  }
+}
+
+const updatePageNav = ({current, prev, next}) => {
+  console.log('updatePageNav', {current, prev, next})
+  const page = fs.readFileSync(current, 'utf8')
+
+  let updatedPage = page.replace(nextorprevRegex, '')
+  if (prev) {
+    console.log('prev find', page.match(prevRegex));
+    updatedPage = `${updatedPage}\n\nPrevious: ${prev.title}`
+  }
+
+  if (next) {
+    updatedPage = `${updatedPage}\n\nNext ${next.title}`
+    console.log('next find', page.match(nextRegex));
+  }
+
+  fs.outputFileSync(current, updatedPage)
+
+}
+
 const pullTitle = /(\w)-([\w-]*).md/;
 misc.forEach(title => {
   const p = `${miscPath}/${title}`
   const n = title.match(pullTitle)[1]
   const t = title.match(pullTitle)[2]
-  toc.push(makeTitle({n, t, p}))
+  const item = makeItem({n, t, p})
+  toc.push(item)
 })
 
 chapters.forEach(title => {
   const p = `${chaptersPath}/${title}`
   const n = title.match(pullTitle)[1]
   const t = title.match(pullTitle)[2]
-  toc.push(makeTitle({n, t, p}))
+  const item = makeItem({n, t, p})
+  toc.push(item)
 })
 
+const updatedToc = tocFile.replace(tocRegex, `# Table Of Content\n\n${toc.map(i => i.title).join('\n\n')}`)
 
-const updatedToc = tocFile.replace(regex, `# Table Of Content\n\n${toc.join('\n\n')}`)
+const pages = toc.length
+toc.forEach((i, index) => {
+  const page = index + 1
+  if (page > 1) {
+    i.prev = {
+      title: toc[index-1].title,
+      path: toc[index-1].path
+    }
+  }
+  if (page < pages) {
+    i.next = {
+      title: toc[index+1].title,
+      path: toc[index+1].path
+    }
+  }
+  // console.log('i', page, pages, i);
+})
+
+toc.forEach(i => {
+  const current = i.path
+  const prev = i.prev
+  const next = i.next
+  updatePageNav({current, prev, next})
+})
 
 fs.outputFileSync(tocPath, updatedToc)
